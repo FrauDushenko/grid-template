baf74d97b00c7c9e290c9ef62a67cc730ac28cda
FrauDushenko <irina.dushenko84@gmail.com>
2020-06-15 15:27:47 +0200

    исправила . убрала repeat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   $(".header-navbar__btn").click(function () {
     $(this).toggleClass("header-navbar__btn--active");
-    // $(".header-navbar__list").toggleClass("header-navbar__list--active");
     $(".header-navbar__list").fadeToggle(500);
   });
 });
